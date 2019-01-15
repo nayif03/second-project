e931a7a2fab6f6d1cbfdfc9edad8717fa57398b6
nayif03 <naif.nayoul@yahoo.com>
2019-01-15 22:38:20 +0100

    done!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 let dog = "Dog";
 let cow = "Cow";
 let chinchilla = "Chinchilla";
-let age1 = document.getElementById('age1');
+age1 = "age1";
+age2 = "age2";
+age3 = "age3";
 
 function calculateAge(animalName, animAge) {
-    age1 = document.getElementById('age1').value;
+    animAge = document.getElementById(animAge).value;
     if (animalName == dog) {
         animalHumanAge = animAge * 7
         document.getElementById('p1').innerHTML = "your dog is " + animalHumanAge + " years old in human years " + "(" + animAge + " dog years)!";
@@ -18,9 +20,31 @@ function calculateAge(animalName, animAge) {
     return;
 }
 
+hide1="hide1";
+hide2="hide2";
+hide3="hide3";
+hidden1="hidden1";
+hidden2="hidden2";
+hidden3="hidden3";
 
+function hide(first,second,third) {
+    document.getElementById(first).style.visibility="hidden";
+    document.getElementById(second).style.visibility="hidden";
+    document.getElementById(third).style.visibility="visible";
+    document.getElementById("back").style.visibility="visible";
+    document.getElementById("heading").style.visibility="hidden";
+    return;
+}
 
+function show() {
+    document.getElementById("hide1").style.visibility="visible";
+    document.getElementById("hide2").style.visibility="visible";
+    document.getElementById("hide3").style.visibility="visible";
+    document.getElementById("back").style.visibility="hidden";
+    document.getElementById(hidden1).style.visibility="hidden";
+    document.getElementById(hidden2).style.visibility="hidden";
+    document.getElementById(hidden3).style.visibility="hidden";
+    document.getElementById("heading").style.visibility="visible";
+    return;
+}
 
-// const test = calculateAge(cow,10);
-// const test1 = calculateAge(dog,5);
-// const test2 = calculateAge(chinchilla,15);
